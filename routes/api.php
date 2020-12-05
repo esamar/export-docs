6a8fc5cddba36a5d6e3cc0bdde867a691c203074
Edinson <ev.samar.c@gmail.com>
2020-12-05 08:59:14 -0500

    Añadiendo cors 4

--- routes/api.php
+++ routes/api.php
@@ -22,5 +22,11 @@ Route::post('updateRange', 'SMIController@updateRange')->name('updateRange');
 
 // Route::post('setState', 'SMIController@setState')->name('setState');
 
-Route::post('setState', array('middleware' => 'cors', 'uses' => 'SMIController@setState'));
+// Route::post('setState', array('middleware' => 'cors', 'uses' => 'SMIController@setState'));
 
+Route::match(['options', 'post'], 'setState', function () {
+    // This will work with the middleware shown in the accepted answer
+Route::post('setState', 'SMIController@setState')->name('setState');
+
+
+})->middleware('cors');
